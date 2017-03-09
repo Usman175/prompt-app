611dbe43b1c1bc44cfbcede5dc591adb16989746
Hamza <Hamzanasim_cs@yahoo.com>
2017-03-09 15:06:28 +0500

    prompt work in lab

---
+++ lab work 9_3_17/app.js
@@ -0,0 +1,74 @@
+// var a = 8;
+// a = a + a + a++ + ++a;
+// a = a-- + ++a + 20
+// document.write(a);
+// console.log(a);
+
+// for(var a=1 ; a<100 ; a++){
+// console.log(table + " x "+a+" = " + (table*a));
+
+// }
+
+
+// var table = +prompt("Enter Value to print Table");
+// console.log(table + " x 1 = " + (table*1));
+// console.log(table + " x 2 = " + (table*2));
+// console.log(table + " x 3 = " + (table*3));
+// console.log(table + " x 4 = " + (table*4));
+// console.log(table + " x 5 = " + (table*5));
+// console.log(table + " x 6 = " + (table*6));
+// console.log(table + " x 7 = " + (table*7));
+// console.log(table + " x 8 = " + (table*8));
+// console.log(table + " x 9 = " + (table*9));
+// console.log(table + " x 10 = " + (table*10));
+// document.write('<br>'+table + " x 1 = " + (table*1));
+// document.write('<br>'+table + " x 2 = " + (table*2));
+// document.write('<br>'+table + " x 3 = " + (table*3));
+// document.write('<br>'+table + " x 4 = " + (table*4));
+// document.write('<br>'+table + " x 5 = " + (table*5));
+// document.write('<br>'+table + " x 6 = " + (table*6));
+// document.write('<br>'+table + " x 7 = " + (table*7));
+// document.write('<br>'+table + " x 8 = " + (table*8));
+// document.write('<br>'+table + " x 9 = " + (table*9));
+// document.write('<br>'+table + " x 10 = " + (table*10));
+
+
+// var a =+prompt("Enter First Value");
+// var operator =prompt("Enter operator (USE ONLY + - * / Operators)");
+// var b =+prompt("Enter second value");
+// if(operator==='+'){
+// result = a+b;
+// document.write('result = '+result );
+
+// }else if(operator==='-'){
+
+// result = b-a;
+// document.write('result = '+result );
+
+
+// }else if(operator==='*'){
+
+// result = a*b;
+// document.write('result = '+result );
+
+// }else if(operator==='/'){
+
+// result = b/a;
+// document.write('result = '+result );
+
+
+// }else {
+// document.write('Enter Wrong Informations' );
+// }
+
+var a = prompt("Enter Name : ");
+var b = prompt("Enter Email : ");
+var c = prompt("Enter Password : ");
+
+document.write('<br>' + "Name : " +a );
+document.write('<br>' + "Email : " +b );
+document.write('<br>' + "Password : " +c );
+
+
+
+
